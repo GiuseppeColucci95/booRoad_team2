6a161d3476848702d478881210fd7c56143ba220
alessandrofalcoo <alefalco.1003@gmail.com>
2025-04-24 15:50:14 +0200

    refactor: reorganize Login component structure for better readability

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,29 +1,34 @@
 export default function Login() {
     return (
         <>
-            <div className="mb-3">
-                <label htmlFor="" className="form-label">Username</label>
-                <input
-                    type="text"
-                    className="form-control"
-                    name=""
-                    id=""
-                    aria-describedby="helpId"
-                    placeholder=""
-                />
-            </div>
-            <div className="mb-3">
-                <label htmlFor="" className="form-label">Password</label>
-                <input
-                    type="text"
-                    className="form-control"
-                    name=""
-                    id=""
-                    aria-describedby="helpId"
-                    placeholder=""
-                />
-            </div>
+            <div className="container">
+                <form>
+                    <div className="mb-3">
+                        <label htmlFor="" className="form-label">Username</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name=""
+                            id=""
+                            aria-describedby="helpId"
+                            placeholder=""
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="" className="form-label">Password</label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            name=""
+                            id=""
+                            aria-describedby="helpId"
+                            placeholder=""
+                        />
+                    </div>
+                    <button className="btn btn-primary" type="submit">Submit</button>
 
+                </form>
+            </div>
         </>
     )
 }
