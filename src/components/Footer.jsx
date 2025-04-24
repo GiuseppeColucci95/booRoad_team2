944adc392beb22552f20a36bfd6c13a6e329d0b0
NavodContrath <navod.contrath@gmail.com>
2025-04-24 16:21:53 +0200

    feat: add Footer component and update index.css for styling

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 text-center text-white mx-3 border-top border-light">
+            <div className="container">
+                <small className="text-white fs-6 fw-bold">&copy; 2025 BooRoad Inc.</small>
+            </div>
+        </footer>
+
+    )
+}
