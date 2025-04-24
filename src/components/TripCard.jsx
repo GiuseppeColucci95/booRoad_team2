99565c07993a22d95c0ccb26131a37343f701acd
GiuseppeColucci95 <colucci539@gmail.com>
2025-04-24 17:13:57 +0200

    feat: add TripsList page and TripCard component for displaying trips

---
+++ src/components/TripCard.jsx
@@ -0,0 +1,15 @@
+export default function TripCard({ trip }) {
+
+  //template
+  return (
+    <div className="col">
+      <div className="card h-100">
+        <img className="card-img-top h-100" src={`/images/${trip.image}`} alt={`${trip.destination} image`} />
+        <div className="card-body">
+          <h5>{trip.destination}</h5>
+          <h6>{trip.date}</h6>
+        </div>
+      </div>
+    </div>
+  );
+}
