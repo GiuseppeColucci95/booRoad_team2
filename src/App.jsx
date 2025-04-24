01da92567fb5287863922efa9a4943a3c03f737d
imnxtamess <davide.morisco.work@gmail.com>
2025-04-24 15:45:08 +0200

    feat: update dependencies and implement routing with DefaultLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
+import Login from "./pages/Login";
 export default function App() {
-
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route Component={DefaultLayout}>
+            <Route path="/login" Component={Login} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
