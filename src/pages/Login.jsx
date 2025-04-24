a0b681e0aa2186e6c77d55c8552feedb80c69d6b
alessandrofalcoo <alefalco.1003@gmail.com>
2025-04-24 15:46:25 +0200

    feat: add username and password input fields to Login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,28 @@
 export default function Login() {
     return (
         <>
+            <div className="mb-3">
+                <label htmlFor="" className="form-label">Username</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    name=""
+                    id=""
+                    aria-describedby="helpId"
+                    placeholder=""
+                />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="" className="form-label">Password</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    name=""
+                    id=""
+                    aria-describedby="helpId"
+                    placeholder=""
+                />
+            </div>
 
         </>
     )
