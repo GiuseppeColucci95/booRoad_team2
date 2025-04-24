d544edd556672cd1a33cc0b2cb9d7512612f7e9c
alessandrofalcoo <alefalco.1003@gmail.com>
2025-04-24 16:13:59 +0200

    refactor: restructure Login component for improved layout and readability

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,32 +2,33 @@ export default function Login() {
     return (
         <>
             <div className="container">
-                <form>
-                    <div className="mb-3" id="username">
-                        <label htmlFor="" className="form-label">Username</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            name=""
-                            id=""
-                            aria-describedby="helpId"
-                            placeholder=""
-                        />
-                    </div>
-                    <div className="mb-3" id="password">
-                        <label htmlFor="" className="form-label">Password</label>
-                        <input
-                            type="password"
-                            className="form-control"
-                            name=""
-                            id=""
-                            aria-describedby="helpId"
-                            placeholder=""
-                        />
-                    </div>
-                    <button id="login" className="btn btn-primary" type="submit">Login</button>
-
-                </form>
+                <div className="card p-4 shadow">
+                    <form>
+                        <div className="mb-3" id="username">
+                            <label htmlFor="" className="form-label">Username</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                name=""
+                                id=""
+                                aria-describedby="helpId"
+                                placeholder=""
+                            />
+                        </div>
+                        <div className="mb-3" id="password">
+                            <label htmlFor="" className="form-label">Password</label>
+                            <input
+                                type="password"
+                                className="form-control"
+                                name=""
+                                id=""
+                                aria-describedby="helpId"
+                                placeholder=""
+                            />
+                        </div>
+                        <button id="login" className="btn btn-primary" type="submit">Login</button>
+                    </form>
+                </div>
             </div>
         </>
     )
