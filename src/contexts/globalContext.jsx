ea2bf67c3f6982f512a5e70b199c48fb8e54bbf8
GiuseppeColucci95 <colucci539@gmail.com>
2025-04-24 16:02:36 +0200

    feat: enhance TripContext with useContext hook and provide trips state

--- src/contexts/globalContext.jsx
+++ src/contexts/globalContext.jsx
@@ -1,15 +1,24 @@
-import { createContext, useState } from "react";
+//react imports
+import { createContext, useContext, useState } from "react";
 
+//define the context
 const TripContext = createContext();
 
+//function to wrap the app
 function TripProvider({ children }) {
 
   const [trips, setTrips] = useState([]);
 
   return (
-    <TripContext.Provider value>
+    <TripContext.Provider value={trips}>
       {children}
     </TripContext.Provider>
   );
+}
+
+//function to use the custom context
+function useTrips() {
+
+  return useContext(TripContext);
 }
 
