8e7de04dc30f676db662b172e68f252887493759
imnxtamess <davide.morisco.work@gmail.com>
2025-04-28 10:57:17 +0200

    refactor: improve Header component structure and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,38 @@
+import { NavLink, Navigate, useLocation } from "react-router-dom";
+
 export default function Header() {
+  const location = useLocation();
   return (
     <>
       <div className="header">
         <div className="menu-sx">
-          <img src="./public/img/logo_1.svg" className="logo" alt="" srcSet="" />
+          <img
+            src="./public/img/logo_1.svg"
+            className="logo"
+            alt=""
+            srcSet=""
+          />
           <h1 className="title">BooRoad</h1>
         </div>
-        <div className="menu-dx">
-          <ul>
-            <li>Home</li>
-            <li>Trips</li>
-            <li>Contacts</li>
-          </ul>
-        </div>
+        {location.pathname === "/" ? null : (
+          <div className="menu-dx">
+            <nav>
+              <NavLink
+                className={({ isActive }) => (isActive ? "active" : undefined)}
+                to={"/trips"}
+              >
+                Home
+              </NavLink>
+
+              <NavLink
+                className={({ isActive }) => (isActive ? "active" : undefined)}
+                to={"/"}
+              >
+                Logout
+              </NavLink>
+            </nav>
+          </div>
+        )}
       </div>
     </>
   );
