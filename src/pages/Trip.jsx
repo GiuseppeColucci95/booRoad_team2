4099aad546b292d0391fbdd90f10dd8e5720b967
alessandrofalcoo <alefalco.1003@gmail.com>
2025-04-28 15:37:09 +0200

    feat: add SearchBar component and integrate search functionality in Trip page style: update Footer and Header components for improved layout refactor: enhance TripCard styling and structure fix: adjust CSS for better responsiveness and visual consistency

--- src/pages/Trip.jsx
+++ src/pages/Trip.jsx
@@ -1,8 +1,60 @@
+import { useParams } from "react-router-dom";
+import { useTrips } from "../contexts/globalContext";
+import { useState } from "react";
+import { useEffect } from "react";
+import SearchBar from "../components/SearchBar";
+
 export default function Trip() {
+
+  //datas
+  const [filteredTravelers, setFilteredTravelers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const { id } = useParams();
+  const tripsData = useTrips();
+  const trip = tripsData[id - 1];
+  const travelers = trip.travelers;
+
+
+  useEffect(() => {
+
+    const filtered = travelers.filter(traveler => {
+      const name = traveler.firstName + " " + traveler.lastName;
+      return name.toLowerCase().includes(searchQuery.toLowerCase());
+    });
+
+    setFilteredTravelers(filtered);
+  }, [searchQuery]);
+
+  console.log(searchQuery);
+
 
   return (
     <>
-      <h1>Single trip page</h1>
+      <section id="jumbotron" className="mb-5">
+        <h1 className="text-center text-white py-3">{trip.destination.toUpperCase()}</h1>
+        <img className="w-100" src={`/images/${trip.image}`} alt={`${trip.title} image`} />
+      </section>
+
+      <section id="travelers" className="mb-5">
+        <div className="container">
+
+          <SearchBar setSearchQuery={setSearchQuery}></SearchBar>
+
+          <div className="card traveler-card">
+
+            <ul className="list-group text-left ">
+              {
+                filteredTravelers.map((traveler, index) => (
+                  <li className="list-group-item travelers border-0 border-bottom rounded-0 d-flex justify-content-between" key={index}>
+                    <div>{`${index + 1}. ${traveler.firstName + ' ' + traveler.lastName}`}</div>
+                    <div><i className="bi bi-trash"></i></div>
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+        </div>
+      </section >
     </>
   );
 }
