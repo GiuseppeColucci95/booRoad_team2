1281f274a088cc764161bcb4d6f42ec026a73173
antonella1986 <antonella.leo.mail@gmail.com>
2025-04-28 15:30:30 +0200

    add style to single trip cards

--- src/components/TripCard.jsx
+++ src/components/TripCard.jsx
@@ -2,8 +2,8 @@ export default function TripCard({ trip }) {
 
   //template
   return (
-    <div className="col h-100">
-      <div className="card h-100">
+    <div className="col h-100 mb-3">
+      <div className="card h-100 trips-list">
         <img className="card-img-top h-100" src={`/images/${trip.image}`} alt={`${trip.destination} image`} />
         <div className="card-body">
           <h5>{trip.destination}</h5>
