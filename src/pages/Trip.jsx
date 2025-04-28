b5b5744abfc4f2f5bd9a6e6fcb171fad9c599ba2
GiuseppeColucci95 <colucci539@gmail.com>
2025-04-28 17:20:37 +0200

    feat: implement add and delete functionality for travelers in Trip component; enhance styling for add button

--- src/pages/Trip.jsx
+++ src/pages/Trip.jsx
@@ -12,21 +12,57 @@ export default function Trip() {
   const { id } = useParams();
   const tripsData = useTrips();
   const trip = tripsData[id - 1];
-  const travelers = trip.travelers;
+  const [travelers, setTravelers] = useState(trip.travelers);
+  const [newTraveler, setNewTraveler] = useState({});
+  const [add, setAdd] = useState(false);
 
-
+  //useEffect
   useEffect(() => {
-
     const filtered = travelers.filter(traveler => {
       const name = traveler.firstName + " " + traveler.lastName;
       return name.toLowerCase().includes(searchQuery.toLowerCase());
     });
 
     setFilteredTravelers(filtered);
-  }, [searchQuery]);
+  }, [searchQuery, travelers]);
 
-  console.log(searchQuery);
+  //function to add a traveler
+  function handleAdd(e) {
+    e.preventDefault();
+    console.log(newTraveler);
 
+    const array = filteredTravelers;
+
+    array.push(newTraveler)
+
+    setTravelers(array);
+    setAdd(false);
+  }
+
+  //function to delete a traveler
+  function handleDelete(index) {
+
+    console.log(index);
+
+    const array = filteredTravelers;
+    const nameToDelete = array[index];
+
+    array.splice(array.indexOf(nameToDelete), 1);
+
+    setTravelers(array);
+  }
+
+  function handleAddState(e) {
+    setAdd(true);
+  }
+
+  function handleNewTraveler(e) {
+    console.log(e.target.value);
+    console.log();
+
+
+    setNewTraveler({ ...newTraveler, [e.target.name]: e.target.value });
+  }
 
   return (
     <>
@@ -47,12 +83,81 @@ export default function Trip() {
                 filteredTravelers.map((traveler, index) => (
                   <li className="list-group-item travelers border-0 border-bottom rounded-0 d-flex justify-content-between" key={index}>
                     <div>{`${index + 1}. ${traveler.firstName + ' ' + traveler.lastName}`}</div>
-                    <div><i className="bi bi-trash"></i></div>
+                    <div onClick={() => handleDelete(index)}><i className="bi bi-trash"></i></div>
                   </li>
                 ))
               }
             </ul>
           </div>
+
+          {
+            !add ?
+              (
+                <button onClick={handleAddState} className="my-3 fs-1 mx-auto" id="btn-add">+</button>
+              )
+              :
+              (
+                <form className="w-50 mx-auto mt-3" onSubmit={handleAdd} action="">
+                  <div className="d-flex flex-column align-items-center justify-content-center gap-2">
+
+                    <div className="d-flex gap-3 w-100">
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="firstName"
+                        id="firstName"
+                        aria-describedby="firstNameId"
+                        placeholder="First Name"
+                        onChange={(e) => handleNewTraveler(e)}
+                      />
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="lastName"
+                        id="lastName"
+                        aria-describedby="lastNameId"
+                        placeholder="Last name"
+                        onChange={(e) => handleNewTraveler(e)}
+                      />
+                      <input
+                        type="number"
+                        className="form-control"
+                        name="phone"
+                        id="phone"
+                        aria-describedby="phoneId"
+                        placeholder="Phone"
+                        onChange={(e) => handleNewTraveler(e)}
+                      />
+                    </div>
+                    <div className="d-flex gap-3 w-100">
+                      <input
+                        type="email"
+                        className="form-control"
+                        name="email"
+                        id="email"
+                        aria-describedby="emailId"
+                        placeholder="e-mail"
+                        onChange={(e) => handleNewTraveler(e)}
+                      />
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="fiscalCode"
+                        id="fiscalCode"
+                        aria-describedby="fiscalCodeId"
+                        placeholder="Fiscal Code"
+                        onChange={(e) => handleNewTraveler(e)}
+                      />
+                    </div>
+
+                    <button className="new mt-1" id="btn-add">ADD NEW TRAVELER</button>
+                  </div>
+                </form>
+              )
+          }
+
+
+
         </div>
       </section >
     </>
