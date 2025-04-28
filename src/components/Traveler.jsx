58aa7eca8886121448140d57bbea4fa4d1d907a3
NavodContrath <navod.contrath@gmail.com>
2025-04-28 19:30:31 +0200

    fix: update styles for TravelerInfo component and improve pin button interaction

--- src/components/Traveler.jsx
+++ src/components/Traveler.jsx
@@ -5,13 +5,12 @@ export default function TravelerInfo({ selectedTraveler, setOpenOverlay }) {
     return (
         <div id="traveler-info">
             <div className="card-info p-4 m-5 position-relative">
-                <div className="pin" onClick={() => { setOpenOverlay(false) }}><i class="bi bi-x-circle"></i></div>
-
+                <div className="pin" onClick={() => { setOpenOverlay(false) }}><i class="bi bi-x"></i></div>
                 <div className="card-body">
-                    <h4 className="card-title">{fullName}</h4>
-                    <p className="card-text"><b>Email: </b>{selectedTraveler.email}</p>
-                    <p className="card-text"><i class="bi bi-telephone-fill"></i> <span className="border-bottom border-dark">{selectedTraveler.phone}</span></p>
-                    <p className="card-text"><b>CF: </b>{selectedTraveler.fiscalCode}</p>
+                    <h4 className="card-title mb-2">{fullName}</h4>
+                    <p className="card-text mb-2"><b className="text-lilac">Email: </b>{selectedTraveler.email}</p>
+                    <p className="card-text mb-2"><i className="bi bi-telephone-fill text-lilac"></i> <span className="border-bottom border-light">{selectedTraveler.phone}</span></p>
+                    <p className="card-text mb-2"><b className="text-lilac">CF: </b>{selectedTraveler.fiscalCode}</p>
                 </div>
             </div>
         </div>
