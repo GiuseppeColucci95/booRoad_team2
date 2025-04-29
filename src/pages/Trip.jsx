3ffb38b516bdf67639b67e9781d4b1d90fa824b7
GiuseppeColucci95 <colucci539@gmail.com>
2025-04-29 14:51:36 +0200

    fix: provide default values for trip properties in Trip component

--- src/pages/Trip.jsx
+++ src/pages/Trip.jsx
@@ -80,11 +80,11 @@ export default function Trip() {
   return (
     <>
       <section id="jumbotron" className="mb-5">
-        <h1 className="text-center text-white py-3">{trip?.destination.toUpperCase()}</h1>
+        <h1 className="text-center text-white py-3">{trip?.destination.toUpperCase() ?? "Puglia"}</h1>
         <div className="text-center mb-4">
-          <h2>{`${trip.date} / ${trip.returnDate}`}</h2>
+          <h2>{`${trip?.date ?? "2023-11-15"} / ${trip?.returnDate ?? "2023-11-15"}`}</h2>
         </div>
-        <img className="w-100" src={`/images/${trip?.image}`} alt={`${trip?.title} image`} />
+        <img className="w-100" src={`/images/${trip?.image ?? "puglia.jpg"}`} alt={`${trip?.title} image`} />
       </section>
 
       <section id="travelers" className="mb-5">
